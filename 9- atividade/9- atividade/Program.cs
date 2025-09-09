4e69cf0896ae2ff8000dc134c7745f1c19c24d28
AnaLuciaSalesBassani <0025910@sesi-es.org.br>
2025-09-09 11:04:49 -0300

    atividade 9 finalizada

---
+++ 9- atividade/9- atividade/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+    Console.WriteLine("Digite o número de maçãs compradas: ");
+
+    // Verifica se a entrada é um número inteiro válido
+    if (!int.TryParse(Console.ReadLine(), out int quantidade) || quantidade < 0)
+    {
+        Console.WriteLine("Por favor, digite um número inteiro não negativo.");
+        return;
+    }
+
+    double precoPorMaca = quantidade < 12 ? 1.30 : 1.00;
+    double custoTotal = quantidade * precoPorMaca;
+
+    Console.WriteLine($"O custo total da compra é: R${custoTotal:F2}");
+}
