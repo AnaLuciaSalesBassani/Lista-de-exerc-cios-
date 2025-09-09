1823b6d1c2bad922d62aed4821aaa4576a5f377a
Ana Lucia <0025910@sesi-es.org.br>
2025-09-09 09:20:19 -0300

    atividade 8 finalizada

---
+++ 8-Atividade/8-Atividade/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero;
+Console.WriteLine("Digite um numero (que não seja zero)");
+numero = int.Parse(Console.ReadLine());
+if (numero == 0)
+{
+    Console.WriteLine("Número inválido.");
+}
+if (numero > 0)
+{
+    Console.WriteLine("Esse número é positivo");
+}
+if (numero < 0)
+{
+    Console.WriteLine("O número é positivo");
+}
+Console.ReadLine();
