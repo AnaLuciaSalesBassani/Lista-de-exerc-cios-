bd193bd2d285f8ad36808b63a0980e86acd9ea5e
Ana Lucia <0025910@sesi-es.org.br>
2025-09-09 08:04:34 -0300

    Atividade 7 finalizada

---
+++ 7-Atividade/7-Atividade/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero;
+
+Console.WriteLine("Olá, digite um número e direi se é maior que 10.");
+   numero = int.Parse(Console.ReadLine());
+if (numero >10)
+{
+    Console.WriteLine("O Valor digitado é maior do que 10");
+
+}
+else
+{
+    Console.WriteLine("O valor digitado não é maior do que 10");
+}
+Console.ReadLine();
